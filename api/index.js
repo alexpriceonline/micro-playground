a537f9cbe5dbb7cfa9ee486cba9103c525dd693f
Alex Price <hi@alexpriceonline.com>
2018-12-18 22:53:11 +0000

    First lambda endpoints

---
+++ api/index.js
@@ -0,0 +1,17 @@
+/**
+ * This file is for development purposes only.
+ * Each micro function will be deployed as a seperate endpoint
+ */
+const { send } = require("micro");
+const { router, get } = require("micro-fork");
+
+const auth = require("./auth");
+const welcome = require("./welcome");
+
+const notfound = (req, res) => send(res, 404, "Not found route");
+
+module.exports = router()(
+  get("/auth", auth),
+  get("/welcome", welcome),
+  get("/*", notfound)
+);
